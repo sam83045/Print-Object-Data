dd6596dfc12a63b811f6c1061808b97ec802a7be
sam83045 <shoaib786k@gmail.com>
2021-06-09 15:57:01 +0530

    1 file modified, 1 added

---
+++ data/data.js
@@ -0,0 +1,8 @@
+export const printData = {
+  firstName: 'Shoaib',
+  lastName: 'Konnur',
+  Education: {
+    SSC: '2005',
+    HSC: '2007'
+  }
+};
--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import LoopOverObject from "./LoopOverObject";
+import LoopOverObject from './LoopOverObject';
+import { printData } from '../data/data';
 
 import './style.css';
-
-const printData = {
-  firstName: 'Shoaib',
-  lastName: 'Konnur',
-  Education: {
-    SSC: '2005',
-    HSC: '2007'
-  }
-};
 
 export default function App() {
   return (
     <div>
       <h1>Print Object Data</h1>
-      <LoopOverObject objData={printData}/>
+      <LoopOverObject objData={printData} />
     </div>
   );
 }
